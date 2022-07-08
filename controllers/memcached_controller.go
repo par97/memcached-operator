b372ae0d2276693f750c3d3a06294a5dc3711942
par97 <102787934+par97@users.noreply.github.com>
2022-07-08 22:44:13 +0800

    add RBAC
    no rbac can not update node label

--- controllers/memcached_controller.go
+++ controllers/memcached_controller.go
@@ -40,6 +40,7 @@ const EMPTY = "EMPTY"
 //+kubebuilder:rbac:groups=cache.example.com,resources=memcacheds,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=cache.example.com,resources=memcacheds/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=cache.example.com,resources=memcacheds/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=nodes,verbs=get;list;patch;watch;update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
